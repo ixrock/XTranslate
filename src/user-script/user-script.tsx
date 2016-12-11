b99b82ff66798d420b1002f3666cfea8f6965c66
Roman <ixrock@gmail.com>
2016-12-12 00:43:24 +0200

    append app's container to top of the body to prevent vertical scrollbar in some rare cases with specific html-layout

--- src/user-script/user-script.tsx
+++ src/user-script/user-script.tsx
@@ -428,5 +428,5 @@ class App extends React.Component<{}, State> {
 // init app
 const appContainer = document.createElement("div");
 appContainer.className = "XTranslate";
-document.body.appendChild(appContainer);
+document.body.insertBefore(appContainer, document.body.firstChild);
 render(<App/>, appContainer);
