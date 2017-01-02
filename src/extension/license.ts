53177894543c325eb403a3ce49cbf80d2f297462
Roman <ixrock@gmail.com>
2017-01-02 15:09:38 +0200

    v.2.1.8 turn off auto extend trial version

--- src/extension/license.ts
+++ src/extension/license.ts
@@ -1,10 +1,6 @@
-import { storage } from "../store/storage";
 import { getPurchases, getSkuDetails, buy } from "./payments";
 import { createStorage } from "../utils/createStorage";
-import { sendMessage } from "./runtime";
-import { MessageType } from "./message";
 import find = require("lodash/find");
-const adsActivated = createStorage<number>('adsActivated');
 const installTime = createStorage<number>('installTime', Date.now(), true);
 const trialPeriodTime = 1000 * 60 * 60 * 24 * 10;
 
@@ -14,17 +10,6 @@ export async function buyApp() {
 
 export async function checkLicense(allowAds = false) {
   if (!trialIsOver() || allowAds) return;
-  if (trialIsOver() && !adsActivated()) {
-    storage.sync.get().then(appState => {
-      var settings = appState.settings;
-      settings.allowAds = true;
-      storage.sync.set({ settings }).then(() => {
-        adsActivated(Date.now());
-        sendMessage({ type: MessageType.APP_STATE, payload: appState });
-      });
-    });
-    return;
-  }
   return getPurchases().then(result => {
     var license = find(result.response.details, { sku: "license", state: "ACTIVE" });
     if (!license) throw new Error("no-license");
