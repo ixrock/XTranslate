2b8c257a911a17341f58c7f93a3cbfa4b1be7c6d
Roman <ixrock@gmail.com>
2022-11-23 23:00:58 +0200

    fix tests
    Signed-off-by: Roman <ixrock@gmail.com>

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,2 @@
-// Setup default configuration for external npm-packages
-import "./src/setup";
-
 // Mock __non_webpack_require__ for tests
 (globalThis as any).__non_webpack_require__ = jest.fn();
